ef9b8ffa93c02adf2c4f7830c36d9670dd34d041
Thomas Taylor <hello@tomtaylor.name>
2023-02-24 18:26:25 +0000

    Update doc comments

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
+/** 
+ * Local (username/password) authentication
+ * @namespace localauth
+ */
 export { default } from './lib/LocalAuthModule.js';
 export { default as PasswordUtils } from './lib/PasswordUtils.js';
