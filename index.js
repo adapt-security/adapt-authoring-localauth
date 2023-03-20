afe0b84e0a29579da763f12423dfd3fbcab9afcb
Thomas Taylor <hello@tomtaylor.name>
2023-03-20 15:23:42 +0000

    Enable eslint and standard ruleset

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-/** 
+/**
  * Local (username/password) authentication
  * @namespace localauth
  */
-export { default } from './lib/LocalAuthModule.js';
-export { default as PasswordUtils } from './lib/PasswordUtils.js';
+export { default } from './lib/LocalAuthModule.js'
+export { default as PasswordUtils } from './lib/PasswordUtils.js'
